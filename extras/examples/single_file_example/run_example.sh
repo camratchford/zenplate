f046ff036d7681e894354389130f3964ea5fcc36
Cam Ratchford <camratchford@gmail.com>
2024-12-15 18:43:39 -0700

    Fixed some problems with the examples

--- extras/examples/single_file_example/run_example.sh
+++ extras/examples/single_file_example/run_example.sh
@@ -3,9 +3,10 @@
 THIS_DIR="$(dirname "$(realpath "$0")")"
 pushd "$THIS_DIR"
 
-zenplate --config-file 'config.yml' \
-    --vars 'title=How do you make a cheeseburger?' \
+zenplate \
+    -v 'title=How do you make a cheeseburger?' \
     --var-file 'vars/vars.yml' \
     --force \
-    'templates/readme_template.md.j2' 'README.md'
+    'templates/readme_template.md.j2' 'output_files/README.md'
 
+popd
